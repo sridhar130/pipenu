dc8b124df043483775496370549f46aa7d6a3cc7
Pavel Murat <murat@fnal.gov>
2024-04-07 21:16:58 -0500

    update

---
+++ ana/scripts/remake_histograms.C
@@ -0,0 +1,40 @@
+//
+//-----------------------------------------------------------------------------
+// assuming STNANA loaded, load it manually
+//-----------------------------------------------------------------------------
+
+void remake_pipenu_histograms() {
+
+  stnana("pipenu","bmup0b0s54r0100","","","murat_pipenu_ana()/save=pipenu.bmup0b0s54r0100.murat_pipenu_ana.0000.hist");
+  stnana("pipenu","bmup2b0s54r0100","","","murat_pipenu_ana()/save=pipenu.bmup2b0s54r0100.murat_pipenu_ana.0000.hist");
+  stnana("pipenu","bmup3b0s54r0100","","","murat_pipenu_ana()/save=pipenu.bmup3b0s54r0100.murat_pipenu_ana.0000.hist");
+  stnana("pipenu","bmup4b0s54r0100","","","murat_pipenu_ana()/save=pipenu.bmup4b0s54r0100.murat_pipenu_ana.0000.hist");
+  stnana("pipenu","bmup5b0s54r0100","","","murat_pipenu_ana()/save=pipenu.bmup5b0s54r0100.murat_pipenu_ana.0000.hist");
+
+  stnana("pipenu","bpip0b0s51r0100","","","murat_pipenu_ana()/save=pipenu.bpip0b0s51r0100.murat_pipenu_ana.0000.hist");
+  stnana("pipenu","bpip2b0s51r0100","","","murat_pipenu_ana()/save=pipenu.bpip2b0s51r0100.murat_pipenu_ana.0000.hist");
+  stnana("pipenu","bpip3b0s51r0100","","","murat_pipenu_ana()/save=pipenu.bpip3b0s51r0100.murat_pipenu_ana.0000.hist");
+  stnana("pipenu","bpip4b0s51r0100","","","murat_pipenu_ana()/save=pipenu.bpip4b0s51r0100.murat_pipenu_ana.0000.hist");
+  stnana("pipenu","bpip5b0s51r0100","","","murat_pipenu_ana()/save=pipenu.bpip5b0s51r0100.murat_pipenu_ana.0000.hist");
+
+  stnana("pipenu","bpip2b0s54r0100","","","murat_pipenu_ana()/save=pipenu.bpip2b0s54r0100.murat_pipenu_ana.0000.hist");
+  stnana("pipenu","bpip3b0s54r0100","","","murat_pipenu_ana()/save=pipenu.bpip3b0s54r0100.murat_pipenu_ana.0000.hist");
+  stnana("pipenu","bpip4b0s54r0100","","","murat_pipenu_ana()/save=pipenu.bpip4b0s54r0100.murat_pipenu_ana.0000.hist");
+  stnana("pipenu","bpip5b0s54r0100","","","murat_pipenu_ana()/save=pipenu.bpip5b0s54r0100.murat_pipenu_ana.0000.hist");
+
+}
+
+//-----------------------------------------------------------------------------
+void remake_pion_stop_histograms() {
+
+  stnana("pipenu","bpip0b0s21r0000","","","murat_spmc_ana()/save=pipenu.bpip0b0s21r0000.murat_spmc_ana.0000.hist");
+  stnana("pipenu","bpip2b0s21r0000","","","murat_spmc_ana()/save=pipenu.bpip2b0s21r0000.murat_spmc_ana.0000.hist");
+  stnana("pipenu","bpip3b0s21r0000","","","murat_spmc_ana()/save=pipenu.bpip3b0s21r0000.murat_spmc_ana.0000.hist");
+  stnana("pipenu","bpip4b0s21r0000","","","murat_spmc_ana()/save=pipenu.bpip4b0s21r0000.murat_spmc_ana.0000.hist");
+  stnana("pipenu","bpip5b0s21r0000","","","murat_spmc_ana()/save=pipenu.bpip5b0s21r0000.murat_spmc_ana.0000.hist");
+
+  stnana("pipenu","bpip2b0s24r0000","","","murat_spmc_ana()/save=pipenu.bpip2b0s24r0000.murat_spmc_ana.0000.hist");
+  stnana("pipenu","bpip3b0s24r0000","","","murat_spmc_ana()/save=pipenu.bpip3b0s24r0000.murat_spmc_ana.0000.hist");
+  stnana("pipenu","bpip4b0s24r0000","","","murat_spmc_ana()/save=pipenu.bpip4b0s24r0000.murat_spmc_ana.0000.hist");
+  stnana("pipenu","bpip5b0s24r0000","","","murat_spmc_ana()/save=pipenu.bpip5b0s24r0000.murat_spmc_ana.0000.hist");
+}
