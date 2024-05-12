87c1ad5625926f9662d99255c8519c791c6c9713
Pavel Murat <pavel.murat@gmail.com>
2024-05-12 12:31:31 -0500

    update

--- ana/Analysis.cc
+++ ana/Analysis.cc
@@ -68,14 +68,14 @@ namespace pipenu {
     ch = new AnalysisChannel("bpip4b0s51r0100","bpip4b0s51r0100");
     ch->fNPOT       = 2.5e8  ;
     ch->fNStops     = 31681. ;
-    ch->fNSimSignal = 100000.;
+    ch->fNSimSignal = 1000000.;
     ch->fNormSF     = (ch->fNStops/ch->fNPOT)*fBR_pienu*(1/ch->fNSimSignal);
     AddChannel(ch);
 
     ch  = new AnalysisChannel("bpip4b0s54r0100","bpip4b0s54r0100");
     ch->fNPOT       = 2.5e8  ;
     ch->fNStops     = 583855.;
-    ch->fNSimSignal = 100000.;
+    ch->fNSimSignal = 1000000.;
     ch->fNormSF     = (ch->fNStops/ch->fNPOT)*fBR_pienu*(1/ch->fNSimSignal);
     AddChannel(ch);
 //-----------------------------------------------------------------------------
@@ -84,17 +84,63 @@ namespace pipenu {
     ch = new AnalysisChannel("bpip5b0s51r0100","bpip5b0s51r0100");
     ch->fNPOT       = 2.5e8  ;
     ch->fNStops     = 17225. ;
-    ch->fNSimSignal = 100000.;
+    ch->fNSimSignal = 1000000.;
     ch->fNormSF     = (ch->fNStops/ch->fNPOT)*fBR_pienu*(1/ch->fNSimSignal);
     AddChannel(ch);
 
     ch  = new AnalysisChannel("bpip5b0s54r0100","bpip5b0s54r0100");
     ch->fNPOT       = 2.5e8  ;
     ch->fNStops     = 617324.;
-    ch->fNSimSignal = 100000.;
+    ch->fNSimSignal = 1000000.;
     ch->fNormSF     = (ch->fNStops/ch->fNPOT)*fBR_pienu*(1/ch->fNSimSignal);
     AddChannel(ch);
+
+//-----------------------------------------------------------------------------
+// DIF
+//-----------------------------------------------------------------------------
+    ch  = new AnalysisChannel("bmup0b0s54r0100","bmup0b0s54r0100");
+    ch->fNPOT       = 5e8*0.501*(1600/1.523359)*15  ;
+    ch->fNormSF     = 1./ch->fNPOT;
+    AddChannel(ch);
  
+    ch  = new AnalysisChannel("bmup2b0s54r0100","bmup2b0s54r0100");
+    ch->fNPOT       = 5e8*0.833*(1600/1.523359)*15  ;
+    ch->fNormSF     = 1./ch->fNPOT;
+    AddChannel(ch);
+ 
+    ch  = new AnalysisChannel("bmup3b0s54r0100","bmup3b0s54r0100");
+    ch->fNPOT       = 5e8*0.997*(1600/1.523359)*15  ;
+    ch->fNormSF     = 1./ch->fNPOT;
+    AddChannel(ch);
+ 
+    ch  = new AnalysisChannel("bmup4b0s54r0100","bmup4b0s54r0100");
+    ch->fNPOT       = 5e8*0.997*(1600/1.523359)*15  ;
+    ch->fNormSF     = 1./ch->fNPOT;
+    AddChannel(ch);
+ 
+    // 's56' : full dataset, huge, ~330M events)
+    ch  = new AnalysisChannel("bmup4b0s56r0100","bmup4b0s56r0100");
+    ch->fNPOT       = 5e8*10*(2000./0.362713)*15  ;
+    ch->fNormSF     = 1./ch->fNPOT;
+    AddChannel(ch);
+
+    // 's66' is a strip from 's56' , T0 > 250 ns
+    ch  = new AnalysisChannel("bmup4b0s66r0100","bmup4b0s66r0100");
+    ch->fNPOT       = 5e8*10*(2000./0.362713)*15  ;
+    ch->fNormSF     = 1./ch->fNPOT;
+    AddChannel(ch);
+ 
+    ch  = new AnalysisChannel("bmup5b0s54r0100","bmup5b0s54r0100");
+    ch->fNPOT       = 5e8*1.000*(1600/1.523359)*15  ;
+    ch->fNormSF     = 1./ch->fNPOT;
+    AddChannel(ch);
+ 
+    // 's56' : full dataset, huge, ultimately ~330M events, now - 20% of that)
+    ch  = new AnalysisChannel("bmup5b0s56r0100","bmup5b0s56r0100");
+    ch->fNPOT       = 5e8*10*(2000./0.265798)*15  ;
+    ch->fNormSF     = 1./ch->fNPOT;
+    AddChannel(ch);
+
   }
 
   Analysis::~Analysis() {
